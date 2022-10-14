e65a433183cb7e99adf880c7e064de4854d2d2a2
Zane Shaw <zshawdev@gmail.com>
2022-10-14 13:55:38 -0400

    Start proof-of-concept of targeting hourly-objects via date filtering

--- src/App.js
+++ src/App.js
@@ -8,6 +8,39 @@ function App() {
   const [city, setCity] = useState("New York City")
   const [results, setResults] = useState(null);
 
+  const formatDate = (date) => {
+    return date.replace("-", "/")
+  }
+
+  const formatTime = (time) => {
+    // return time
+    if (time > 12) {
+      return `${time - 12}PM`
+    } else {
+      return `${time}AM`
+    } 
+  }
+
+  const showDates = (object) => {
+    let arr = [];
+    let hoursOfSelectedDay = [];
+    for (let i = 0; i < object.length - 1; i++) {
+      let date = object[i].dt_txt.slice(0, 10)
+      if (!arr.includes(date)) {
+        arr.push(date);
+      }
+    }
+    let copyObject = object.slice();
+    for (let i = 0; i < copyObject.length - 1; i++) {
+      let hourlyItem = copyObject[i].dt_txt.slice(0, 10)
+      let selectedDate = arr[1]
+      if (hourlyItem.includes(selectedDate)) {
+        hoursOfSelectedDay.push(copyObject[i]);
+      }
+    }
+    return console.log(hoursOfSelectedDay);
+  }
+
   useEffect(() => {
     fetch("https://pro.openweathermap.org/data/2.5/forecast/hourly?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
       .then(res => res.json())
@@ -18,6 +51,7 @@ function App() {
           } else {
             setIsLoaded(true);
             setResults(result);
+            console.log(showDates(results.list))
           }
         },
         (error) => {
@@ -45,6 +79,7 @@ function App() {
             <h3>{results.list[0].weather[0].main}</h3>
             <p>Feels like {results.list[0].main.feels_like}°C</p>
             <i><p>{results.city.name}, {results.city.country}</p></i>
+            <i><p>{formatDate(results.list[0].dt_txt.slice(5, 10))} | {formatTime(results.list[0].dt_txt.slice(11, 13))}</p></i>
           </>}
         </div>
       </div>
